e3db81287b9e5b39f483d9f6a67ae80aa11c3a60
andreu55 <anduwet2@gmail.com>
2017-05-10 18:54:41 +0200

    Añadida tabla

--- examples/andreu_test.php
+++ examples/andreu_test.php
@@ -2,6 +2,8 @@
 
 $nombre_pdf = 'factura.pdf';
 
+$datos_taxo = ['TAXO Valoración, S.L.', 'Avda. de Aragón 30 F 13'];
+
 if (isset($_POST['id'])) {
 	$id = $_POST['id'];
 	$horas = $_POST['horas'];
@@ -68,12 +70,15 @@ $pdf->Cell(90, 0, 'Andreu García Martínez', 0, 0, 'L', 0, '', 1);
 $pdf->SetFont('times', 'BI', 19);
 $pdf->Cell(90, 0, 'Fac. ' . $id, 0, 1, 'R', 0, '', 1);
 
-$pdf->Cell(0, 2, '', 'T', 1);
+$pdf->Cell(0, 2, '', 'T', 1); // Linea separadora
 // $pdf->Cell(0, 0, '', '', 1);
 
 // Mis datos
 $pdf->SetFont('times', 'I', 15);
-$pdf->Cell(90, 0, 'C/Cienfuegos 16 - 3', 0, 0, 'L', 0, '', 1);
+
+$pdf->Cell(0, 0, 'NIF: 29196333H', 0, 1, 'L', 0, '', 1);
+
+$pdf->Cell(90, 0, 'Calle Cienfuegos 16, pta 3', 0, 0, 'L', 0, '', 1);
 $pdf->Cell(90, 0, '622 666 125', 0, 1, 'R', 0, '', 1);
 
 $pdf->Cell(90, 0, '46007, Valencia.', 0, 0, 'L', 0, '', 1);
@@ -86,9 +91,58 @@ $pdf->SetFont('times', 'B', 15);
 $pdf->Cell(90, 12, 'Facturar a:', 0, 0, 'L', 0, '', 1);
 $pdf->Cell(90, 12, 'Fecha. ' . $fecha, 0, 1, 'R', 0, '', 1);
 
+// Datos cliente aqui
 $pdf->SetFont('times', 'I', 15);
-$pdf->Cell(90, 0, 'TAXO Valoración, S.L.', 0, 1, 'L', 0, '', 1);
-$pdf->Cell(90, 0, 'Avda. de Aragón 30 F 13', 0, 1, 'L', 0, '', 1);
+$pdf->Cell(90, 0, $datos_taxo[0], 0, 1, 'L', 0, '', 1);
+$pdf->Cell(90, 0, $datos_taxo[1], 0, 1, 'L', 0, '', 1);
+
+$pdf->Ln();
+
+// bgcolor="#cccccc" colspan="2"
+
+$html = '<table border="1" cellspacing="0" cellpadding="5">
+	<tr>
+		<th><b>Concepto</b></th>
+		<th align="center"><b>Cant.</b></th>
+		<th align="center"><b>Base Unit.</b></th>
+		<th align="right"><b>Importe</b></th>
+	</tr>
+	<tr>
+		<td>Desarrollo web y bla bla</td>
+		<td align="center">'.$horas.'</td>
+		<td align="center">15</td>
+		<td>4B</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td bgcolor="#0000FF" color="yellow" align="center">A2 € &euro; &#8364; &amp; è &egrave;<br/>A2 € &euro; &#8364; &amp; è &egrave;</td>
+		<td bgcolor="#FFFF00" align="left"><font color="#FF0000">Red</font> Yellow BG</td>
+		<td>4C</td>
+	</tr>
+	<tr>
+		<td>1A</td>
+		<td rowspan="2" colspan="2" bgcolor="#FFFFCC">2AA<br />2AB<br />2AC</td>
+		<td bgcolor="#FF0000">4D</td>
+	</tr>
+	<tr>
+		<td>1B</td>
+		<td>4E</td>
+	</tr>
+	<tr>
+		<td>1C</td>
+		<td>2C</td>
+		<td>3C</td>
+		<td>4F</td>
+	</tr>
+</table>';
+
+// output the HTML content
+$pdf->writeHTML($html, true, false, true, false, '');
+
+
+
+
+
 
 
 $pdf->Cell(0, 0, 'Has trabajado ' . $horas . ' horas', 0, 1, 'C', 1, '', 0);
